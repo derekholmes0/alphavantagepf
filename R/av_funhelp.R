09007a4d5e2502e6a9c21df2633d0c65e9561686
derekholmes0 <49367667+derekholmes0@users.noreply.github.com>
2026-01-06 10:54:52 -0600

    READMEs done

--- R/av_funhelp.R
+++ R/av_funhelp.R
@@ -2,7 +2,7 @@
 #'
 #' @name av_funhelp
 #'
-#' @param av_fun A Alpha Vantage function name.
+#' @param av_fun_grep A Alpha Vantage function name or portions of one.
 #' For a list of parameters, visit the [Alpha Vantage API documentation](https://www.alphavantage.co/documentation/).
 #'
 #' @returns Help Text and default parameters.
@@ -17,12 +17,22 @@
 #' }
 #'
 #' @export
-av_funhelp <- function(av_fun) {
-    subdf <- av_funcmap[get("av_fn")==toupper(av_fun),]
-    make_plist <- function(wh) {
-      r_params <- subdf[get("ro")==wh,]
-      paste0(wh,"> ",r_params$paramname,data.table::fifelse(is.na(r_params$def_value),"",paste0(" (default: ",r_params$def_value,")")),"\n",collapse="")
-      }
-    helptext <- cat(paste0("Function: ",av_fun,"\nCategory: ",unique(subdf$category),"\n\nParameters:\n",make_plist("R"),make_plist("O")))
+av_funhelp <- function(av_fun_grep="") {
+    if(av_fun_grep=="") {
+      u1<-av_funcmap[,.N,by=.(category,av_fn)][!(category=="ta" | category=="default")]
+      u1<-u1[order(category,av_fn)][,`no`:=.I-min(.I),by=c("category")]
+      return(data.table::dcast(u1,no ~ category,value.var="av_fn",fill=""))
+    }
+    subdf <- av_funcmap[grepl(toupper(av_fun_grep),get("av_fn")),]
+    make_plist <- function(indta,wh) {
+      r_params <- indta[get("ro")==wh,]
+      if(nrow(r_params)>0)
+        return(paste0(wh,"> ",r_params$paramname,data.table::fifelse(is.na(r_params$def_value),"",paste0(" (default: ",r_params$def_value,")")),"\n",collapse=""))
+    }
+    helptestlist <- lapply(unique(subdf$av_fn), function(tfn) {
+                      tdta = subdf[get("av_fn")==tfn,];
+                      paste0("Function: ",tfn,"\nCategory: ",unique(tdta$category),"\n\nParameters:\n",make_plist(tdta,"R"),make_plist(tdta,"O"))
+                })
+    helptext<-cat(paste(helptestlist,collapse="==========\n"))
     return(helptext)
 }
